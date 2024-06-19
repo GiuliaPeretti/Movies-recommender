8d195f3da3809ab045bfcec949d62e4958fe4e7f
GiuliaPeretti <giulia.peretti12@gmail.com>
2024-06-19 17:04:20 +0200

    better output

--- grInterface.py
+++ grInterface.py
@@ -1,15 +1,14 @@
 import gradio as gr
 import pandas as pd
 def recommend(movie):
-    print(movie)
     index=movies[movies['title']==movie].index[0]
-    print(index)
-    print(similarity.iloc[[index]])
     distances=sorted(list(enumerate(similarity.iloc[index-1])), reverse=True, key = lambda x: x[1])
-    result=[]
+    result=""
+    c=1
     for i in distances[1:6]:
-        result.append(movies.iloc[i[0]].title)
-    return(str(result))
+        result = result + str(c) + ") " + movies.iloc[i[0]].title + "\n" 
+        c+=1
+    return(result)
 
 
 similarity=pd.read_csv("similarity.csv")
@@ -17,9 +16,10 @@ movies=pd.read_csv("tmdb_5000_movies.csv")
 titles=movies['original_title'].tolist()
 
 with gr.Blocks() as demo:
-    gr.Markdown("Start typing below and then click **Run** to see the output.")
+    gr.Markdown("Choose a movie you liked:")
     inp=gr.Dropdown(titles)
     btn = gr.Button("Run")
+    gr.Markdown("Movies recommended:")
     out=gr.TextArea()
     btn.click(fn=recommend, inputs=inp, outputs=out)
 
